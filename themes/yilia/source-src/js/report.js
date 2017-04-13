514171ea8041ae498ab4cd94fb082d606edfd43c
miaopei <miaopei63@163.com>
2017-04-13 15:50:40 +0800

    delete litten.me url

--- themes/yilia/source-src/js/report.js
+++ themes/yilia/source-src/js/report.js
@@ -17,7 +17,7 @@ if (window.BJ_REPORT) {
 		uin: window.location.origin,          
 		combo: 0,                             
 		delay: 1000,                          
-		url: "//litten.me:9005/badjs/",       
+		//url: "//litten.me:9005/badjs/",       
 		ignore: [/Script error/i],           
 		random: 1,                            
 		repeat: 500000,                         
